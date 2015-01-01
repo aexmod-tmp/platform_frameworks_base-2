98bf1e29d94d284d5f1bce950249ff8c25e7e70c
Evisceration <eviscerationls@gmail.com>
2015-01-01 14:29:50 +0000

    QuickSettings: Add Haptic Feedback to tiles [1/2]
    Change-Id: I792a00a2689a572110963db00362f8e9d77b316e
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -242,5 +242,6 @@ public class SecureSettings {
         Settings.Secure.HEARING_AID_SYSTEM_SOUNDS_ROUTING,
         Settings.Secure.ACCESSIBILITY_FONT_SCALING_HAS_BEEN_CHANGED,
         Settings.Secure.ADVANCED_REBOOT,
+        Settings.Secure.QUICK_SETTINGS_TILES_VIBRATE,
     };
 }
