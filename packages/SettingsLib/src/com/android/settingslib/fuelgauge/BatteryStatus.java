7e2309218ad39671f0ad2e919faa0872d1a1d77e
Anushek Prasal <anushekprasal@gmail.com>
2019-12-05 21:16:13 +0530

    BatteryService: Add Warp charging support
    Signed-off-by: firebird11 <hbgassel@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I86c308dcd9862c97c47917dc2d910215a34e7bab
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
+++ packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
@@ -28,6 +28,7 @@ import static android.os.BatteryManager.EXTRA_PLUGGED;
 import static android.os.BatteryManager.EXTRA_PRESENT;
 import static android.os.BatteryManager.EXTRA_STATUS;
 import static android.os.BatteryManager.EXTRA_DASH_CHARGER;
+import static android.os.BatteryManager.EXTRA_WARP_CHARGER;
 
 import android.content.Context;
 import android.content.Intent;
@@ -49,6 +50,7 @@ public class BatteryStatus {
     public static final int CHARGING_REGULAR = 1;
     public static final int CHARGING_FAST = 2;
     public static final int CHARGING_DASH = 3;
+    public static final int CHARGING_WARP = 4;
 
     public final int status;
     public final int level;
@@ -61,6 +63,7 @@ public class BatteryStatus {
     public final Optional<Boolean> incompatibleCharger;
     public final float temperature;
     public final boolean dashChargeStatus;
+    public final boolean warpChargeStatus;
 
     public static BatteryStatus create(Context context, boolean incompatibleCharger) {
         final Intent batteryChangedIntent = BatteryUtils.getBatteryIntent(context);
@@ -70,7 +73,7 @@ public class BatteryStatus {
 
     public BatteryStatus(int status, int level, int plugged, int chargingStatus,
             int maxChargingWattage, boolean present, int maxChargingCurrent, int maxChargingVoltage,
-            float temperature, boolean dashChargeStatus) {
+            float temperature, boolean dashChargeStatus, boolean warpChargeStatus) {
         this.status = status;
         this.level = level;
         this.plugged = plugged;
@@ -82,6 +85,7 @@ public class BatteryStatus {
         this.incompatibleCharger = Optional.empty();
         this.temperature = temperature;
         this.dashChargeStatus = dashChargeStatus;
+        this.warpChargeStatus = warpChargeStatus;
     }
 
 
@@ -102,6 +106,7 @@ public class BatteryStatus {
         present = batteryChangedIntent.getBooleanExtra(EXTRA_PRESENT, true);
         temperature = batteryChangedIntent.getIntExtra(EXTRA_TEMPERATURE, -1);
         dashChargeStatus = batteryChangedIntent.getBooleanExtra(EXTRA_DASH_CHARGER, false);
+        warpChargeStatus = batteryChangedIntent.getBooleanExtra(EXTRA_WARP_CHARGER, false);
 
 
         this.incompatibleCharger = incompatibleCharger;
@@ -175,6 +180,7 @@ public class BatteryStatus {
         final int fastThreshold = context.getResources().getInteger(
                 R.integer.config_chargingFastThreshold);
         return dashChargeStatus ? CHARGING_DASH :
+                warpChargeStatus ? CHARGING_WARP :
                 maxChargingWattage <= 0 ? CHARGING_UNKNOWN :
                 maxChargingWattage < slowThreshold ? CHARGING_SLOWLY :
                         maxChargingWattage > fastThreshold ? CHARGING_FAST :
