a63a4fb9661bc741f40e1d3ad302718fca82089b
jhonboy121 <alfredmathew05@gmail.com>
2021-12-07 17:04:23 +0800

    Squashed navigation/gestures patches
    base: allow hiding navbar [1/2]
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: str-br <alexfinhart@gmail.com>
    
    SystemUI: allow changing the length of gesture navbar [1/2]
    
    * forward ported from 11 and improved a lot by delegating all the dirty work to controllers and using LayoutParams for changing the width instead of screwing around in onDraw.
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: str-br <alexfinhart@gmail.com>
    
    SystemUI: Improve gesture bar length [1/2]
    
    * Merge hidden into length settings.
    * Cleanup useless configs in overlay.
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: str-br <alexfinhart@gmail.com>
    
    Gestural: Improvement [1/2]
    
    Signed-off-by: 703joko <703joko@gmail.com>
    Signed-off-by: str-br <alexfinhart@gmail.com>
    
    SystemUI: Update additional navbar dimens for Android 13
    
    The gesture navigation bar is now more prominent in Android 13 and navigation_home_handle_width increased to 108dp.
    Update the steps in the navbar length seekbar to reflect this.
    
    Change-Id: Ic019afe5608560e5df4ecde651b2e18747d16a37
    Co-authored-by: NurKeinNeid <mralexman3000@gmail.com>
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Co-authored-by: 703joko <703joko@gmail.com>
    Signed-off-by: str-br <alexfinhart@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -120,6 +120,7 @@ LOCAL_REQUIRED_MODULES := \
     NavigationBarModeGesturalOverlayExtraWideBack \
     TransparentNavigationBarOverlay \
     NotesRoleEnabledOverlay \
+    NavigationBarModeGesturalOverlayFS \
     preinstalled-packages-platform-overlays.xml
 
 include $(BUILD_PHONY_PACKAGE)
