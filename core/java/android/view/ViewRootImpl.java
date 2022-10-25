555bab42ed4a4524a798997a71727f1b797f8ae3
Akash Srivastava <akashniki@gmail.com>
2022-10-26 03:17:04 +0700

    core: Port CLO's Framework Boost
    * This works just as well as it does on CLO. Ofc relies on CLO's Perf libs
    
    [DennySPb]: Upstream to A14 and squash with other perf commits:
    
    perf: Add lock protection for initialization in ScrollOptimizer
    BoostFramework: Guard it with overlay
    OverScroller: Import BoostFramework.ScrollOptimizer
    core: touch response optimizations
    add VSYNC scheduled flag and avoid two doFrame calls in one period
    make ui touch optimization work normally.
    avoid duplicate doFrame calls.
    BoostFramework: Add some missing parts for scroll optimizer
    Update frame interval when refresh rate changes
    Reduce redundant calls of frame interval update
    refactor pre-rendering feature for BLASTBufferQueue
    Perf:Fix the issue that activity boost duration abnormal.
    Perf: Activity boost optimization.
    perf: Add Rotation boosts, based on ShellTransitions.
    perf: Add Transition Type check.
    perf: recover the pre-rendering feature in the U
    Fix frame timelines length on USE_VSYNC = false
    Set default frameTimelinesLength to 1
    perf: disable pre-rendering feature for some multi-layer cases
    
    Co-authored-by: DennySPb <dennyspb@gmail.com>

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -155,6 +155,7 @@ import android.os.UserHandle;
 import android.sysprop.DisplayProperties;
 import android.text.TextUtils;
 import android.util.AndroidRuntimeException;
+import android.util.BoostFramework.ScrollOptimizer;
 import android.util.DisplayMetrics;
 import android.util.EventLog;
 import android.util.IndentingPrintWriter;
@@ -2295,6 +2296,7 @@ public final class ViewRootImpl implements ViewParent,
         mBlastBufferQueue = new BLASTBufferQueue(mTag, mSurfaceControl,
                 mSurfaceSize.x, mSurfaceSize.y, mWindowAttributes.format);
         mBlastBufferQueue.setTransactionHangCallback(sTransactionHangCallback);
+        ScrollOptimizer.setBLASTBufferQueue(mBlastBufferQueue);
         Surface blastSurface = mBlastBufferQueue.createSurface();
         // Only call transferFrom if the surface has changed to prevent inc the generation ID and
         // causing EGL resources to be recreated.
@@ -9144,6 +9146,7 @@ public final class ViewRootImpl implements ViewParent,
     }
 
     void doProcessInputEvents() {
+        ScrollOptimizer.setBLASTBufferQueue(mBlastBufferQueue);
         // Deliver all pending input events in the queue.
         while (mPendingInputEventHead != null) {
             QueuedInputEvent q = mPendingInputEventHead;
@@ -9158,6 +9161,10 @@ public final class ViewRootImpl implements ViewParent,
                     mPendingInputEventCount);
 
             mViewFrameInfo.setInputEvent(mInputEventAssigner.processEvent(q.mEvent));
+
+            if (q.mEvent instanceof MotionEvent) {
+                ScrollOptimizer.setMotionType(((MotionEvent)q.mEvent).getActionMasked());
+            }
 
             deliverInputEvent(q);
         }
