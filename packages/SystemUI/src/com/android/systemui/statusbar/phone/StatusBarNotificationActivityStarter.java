85767762754d999574a8bdff8a0f036ac96dfe22
DennySPb <DennySPb@gmail.com>
2023-12-08 20:44:29 +0300

    Lalala
    Change-Id: I8a38a2c437b9ae04a99dadd9c8e66aad00d4bb77

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarNotificationActivityStarter.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarNotificationActivityStarter.java
@@ -124,6 +124,7 @@ class StatusBarNotificationActivityStarter implements NotificationActivityStarte
     private final MetricsLogger mMetricsLogger;
     private final StatusBarNotificationActivityStarterLogger mLogger;
 
+    private final CentralSurfaces mCentralSurfaces;
     private final NotificationPresenter mPresenter;
     private final ShadeViewController mShadeViewController;
     private final NotificationShadeWindowController mNotificationShadeWindowController;
@@ -160,6 +161,7 @@ class StatusBarNotificationActivityStarter implements NotificationActivityStarte
             ActivityIntentHelper activityIntentHelper,
             MetricsLogger metricsLogger,
             StatusBarNotificationActivityStarterLogger logger,
+            CentralSurfaces centralSurfaces,
             OnUserInteractionCallback onUserInteractionCallback,
             NotificationPresenter presenter,
             ShadeViewController shadeViewController,
@@ -195,6 +197,7 @@ class StatusBarNotificationActivityStarter implements NotificationActivityStarte
         mFeatureFlags = featureFlags;
         mMetricsLogger = metricsLogger;
         mLogger = logger;
+        mCentralSurfaces = centralSurfaces;
         mOnUserInteractionCallback = onUserInteractionCallback;
         mPresenter = presenter;
         mShadeViewController = shadeViewController;
